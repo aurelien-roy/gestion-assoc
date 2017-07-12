1b3dffb52e357a2da091bc83fde00514fdf7da4e
johan-gras <johan.gras-sub@outlook.com>
2017-07-12 13:29:13 +0200

    Setup Vue/Laravel

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <header class="hide-sm">
+            <div class="row wrapped push-center">
+                <div class="brand-logo" style="line-height: 103px; font-size:2em; text-align: center; border: 1px solid #333;">LOGO</div>
+                <nav class="auto row">
+                    <div class="col col-12 row align-middle">
+                        <div class="col col-12 row around align-middle">
+                            <a href="#"><i class="dashboard w24"></i>Dashboard</a>
+                            <a href="#"><i class="activities w24"></i>Activités</a>
+                            <a href="#"><i class="members w24"></i>Membres</a>
+                            <a href="#"><i class="payments w24"></i>Comptabilité</a>
+                            <a href="#"><i class="settings w24 m0"></i></a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <div id="action_bar">
+            <div class="row action_bar-visible">
+                <a href="#" class="show-sm menu-toggle">
+                    <i class="activities"></i>
+                    <div class="arrow down"></div>
+                </a>
+                <div class="action_bar-content">
+                    TEST
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" src="../../sass/navbar.scss"></style>
+<script>
+    
+    export default{
+        data(){
+            return {}
+        },
+        components: {},
+        
+        methods: {},
+        
+        computed: {},
+        
+        watch: {},
+        
+        mounted(){
+            
+        }
+    }
+
+</script>
--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -3,9 +3,6 @@ import VueRouter from 'vue-router'
 
 import Login from '../views/Auth/Login.vue'
 import Register from '../views/Auth/Register.vue'
-import RecipeIndex from '../views/Recipe/Index.vue'
-import RecipeShow from '../views/Recipe/Show.vue'
-import RecipeForm from '../views/Recipe/Form.vue'
 import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
@@ -13,10 +10,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
 	mode: 'history',
 	routes: [
-		{ path: '/', component: RecipeIndex },
-		{ path: '/recipes/create', component: RecipeForm, meta: { mode: 'create' }},
-		{ path: '/recipes/:id/edit', component: RecipeForm, meta: { mode: 'edit' }},
-		{ path: '/recipes/:id', component: RecipeShow },
+		/*{ path: '/', component: RecipeIndex },*/
 		{ path: '/register', component: Register },
 		{ path: '/login', component: Login },
 		{ path: '/not-found', component: NotFound },
