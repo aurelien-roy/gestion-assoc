36d44c739b237d41b41df6b88a3fa324f0e47e7a
johan-gras <johan.gras-sub@outlook.com>
2017-07-26 18:44:24 +0200

    Laravel Passport installation

--- app/User.php
+++ app/User.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
@@ -24,11 +25,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'api_token'
+        'password', 'remember_token',
     ];
-
-    public function recipes()
-    {
-        return $this->hasMany(Recipe::class);
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-Route::post('login', 'AuthController@login');
-Route::post('logout', 'AuthController@logout');
-Route::post('register', 'AuthController@register');
-
-Route::resource('recipes', 'RecipeController');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
