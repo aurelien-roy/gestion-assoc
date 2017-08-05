d06ef3a5f2c12097b7cc212e4a10f73b594ca2e9
Aurelien Roy <aurelien_roy@outlook.com>
2017-08-05 15:43:02 +0200

    Nouvelle UI Activités

---
+++ resources/assets/js/components/ColorPicker.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="relative">
+        <div class="disc colorful" @click="togglePanel" :class="[currentColor + '-sheet']"></div>
+        <transition name="slide-fade">
+            <div ref="panel" class="panel" v-show="panel_opened" @focusout="closePanel">
+                <div class="disc colorful" v-for="c in colors" :class="[c + '-sheet']" @click="setColor(c)"></div>
+            </div>
+        </transition>
+    </div>
+</template>
+<style lang="scss">
+    .disc{
+        width: 30px;
+        height: 30px;
+        border-radius: 30px;
+        cursor: pointer;
+    }
+
+    .panel{
+        top: 0px;
+        right: 35px;
+        position: absolute;
+        background: #fafafa;
+        border-radius:90px;
+        box-shadow: 0px 0px 8px -3px rgba(0,0,0,0.5);
+        height: 35px;
+        padding-left: 10px;
+        padding-right:10px;
+        display:flex;
+    }
+
+    .panel .disc {
+        box-sizing: content-box;
+        border: 5px solid;
+        border-color: #fafafa !important;
+        margin: auto;
+        width: 20px;
+        height: 20px;
+        transition: transform 0.3s;
+    }
+
+    .panel .disc:hover {
+        transform: scale(1.2);
+    }
+
+
+    .slide-fade-enter-active {
+        transition: all .1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-leave-active {
+        transition: all .1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to{
+
+        transform: translateX(20px);
+        opacity: 0;
+    }
+</style>
+<script>
+    
+    export default {
+        data() {
+            return {
+                panel_opened: false,
+                currentColor: 'red',
+                colors: ['red', 'orange', 'yellow', 'green', 'aqua', 'blue', 'purple']
+            }
+        },
+        components: {},
+        
+        methods: {
+            togglePanel(){
+                this.panel_opened = !this.panel_opened;
+                if(this.panel_opened){
+                    this.$refs.panel.focus();
+                }
+            },
+
+            closePanel(){
+                this.panel_opened = false;
+            },
+
+            setColor(color){
+                this.currentColor = color;
+                this.closePanel();
+            }
+        },
+        
+        computed: {},
+        
+        watch: {},
+        
+        mounted() {
+            
+        }
+    }
+
+</script>
---
+++ resources/assets/js/components/ListHeadChoice.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="row sort-by list-header">
+        <p class="title">{{ title }} :</p>
+        <p class="types auto">
+            <a v-for="c in choices" href="#" @click.prevent="choose(c)" :class="{selected: value === c}">{{ c }}</a>
+        </p>
+    </div>
+</template>
+<style lang="scss">
+
+    .sort-by{
+        height: 36px;
+        padding: 0px 16px;
+        font-size: 0.875rem;
+        line-height: 36px;
+        display: flex;
+
+        background: rgba(234, 234, 234, 0.95);
+
+        p{
+            display: inline-block;
+
+            &.title{
+                 padding-right: 16px;
+            }
+
+            &.types{
+                 text-align: right;
+                 font-family: 'Lato', sans-serif;
+                 font-weight: 300;
+                 font-size: 1rem;
+
+                a{
+                    margin-left: 25px;
+
+                    &.selected{
+                         font-weight: 700;
+                    }
+                }
+            }
+        }
+    }
+
+
+</style>
+<script>
+    
+    export default {
+        data() {
+            return {
+            }
+        },
+        components: {},
+        
+        methods: {
+            choose(c){
+                this.$emit('input', c);
+            }
+        },
+        
+        computed: {},
+        
+        watch: {},
+        
+        mounted() {},
+
+        props: {
+            title: {
+                type: String
+            },
+
+            choices: {
+                type: Array
+            },
+
+            value: {
+                type: String
+            }
+        }
+    }
+
+</script>
