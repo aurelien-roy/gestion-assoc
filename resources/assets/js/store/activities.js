0989add0945199b578d8b89c21fd51cd3b35141b
johan-gras <johan.gras-sub@outlook.com>
2017-08-14 23:03:08 +0200

    Nouveau système de store avec getters réactifs

--- resources/assets/js/store/activities.js
+++ resources/assets/js/store/activities.js
@@ -1,62 +1,74 @@
 import { rand, Time } from '../helpers/math'
-import ajaxStore from '../helpers/ajax-store'
+import Store from '../helpers/store'
+import Vue from 'vue'
 
-let store = {
+export default new Store({
     
     state: {
-        activities: []
+        activities: {},
+    },
+    
+    getters: {
+        //Obtient une activité grâce à l'identifiant
+        get: (store, period, id) => {
+            return store.state.activities[period].find(a => a.id === id);
+        },
+    
+        activitiesByPeriod: (store, period) => {
+            let activities = store.state.activities[period]
+            return activities !== undefined ? activities : [];
+        },
         
     },
-
-    //Génère une nouvelle activitée
-    genActivity(){
-        return {
-            id: undefined,
-            members: [],
-            name: '',
-            level: '',
-            age: '',
-            day: undefined,
-            time_begin: undefined,
-            time_end: undefined,
-            place: '',
-            teacher: '',
-            effectif_max: undefined,
-            effectif_current: undefined,
-            color: '',
-            makeCopy(){
-                let copy = {};
-                Object.assign(copy, this);
-                return copy;
+    
+    methods: {
+    
+        //Génère une nouvelle activitée
+        genActivity() {
+            return {
+                id: undefined,
+                members: [],
+                name: '',
+                level: '',
+                age: '',
+                day: undefined,
+                time_begin: undefined,
+                time_end: undefined,
+                place: '',
+                teacher: '',
+                effectif_max: undefined,
+                effectif_current: undefined,
+                color: '',
+                makeCopy() {
+                    let copy = {};
+                    Object.assign(copy, this);
+                    return copy;
+                }
+            }
+        },
+    
+        //Génère des activités de test
+        generateActivity(period) {
+            let act = ['Classique', 'Jazz', 'Street', 'Ragga', 'Eveil'];
+            let nvx = ['', 'Adultes', '10-12 ans', '5-6 ans'];
+            let clr = ['red', 'blue', 'yellow', 'green', 'aqua', 'purple', 'orange'];
+        
+            for (let i = 0; i < 10; i++) {
+            
+                let a = this.genActivity();
+                a.id = this.state.activities[period].length;
+                a.name = act[rand(0, 4)];
+                a.age = nvx[rand(0, 3)];
+                a.day = rand(0, 6);
+                a.time_begin = new Time(rand(9, 16), 0);
+                a.time_end = a.time_begin.clone().addHours(rand(1, 2));
+                a.teacher = 'Jackie';
+                a.color = clr[rand(0, 6)];
+                
+                this.state.activities[period].push(a);
             }
         }
-    },
-
-    //Génère des activités de test
-    generateActivity(){
-        let act = ['Classique', 'Jazz', 'Street', 'Ragga', 'Eveil'];
-        let nvx = ['', 'Adultes', '10-12 ans', '5-6 ans'];
-        let clr = ['red', 'blue', 'yellow', 'green', 'aqua', 'purple', 'orange'];
-
-        for(let i = 0; i < 10; i ++) {
     
-            let a = this.genActivity();
-            a.id = this.state.activities.length;
-            a.name = act[rand(0, 4)];
-            a.age = nvx[rand(0, 3)];
-            a.day = rand(0, 6);
-            a.time_begin = new Time(rand(9, 16), 0);
-            a.time_end = a.time_begin.clone().addHours(rand(1, 2));
-            a.teacher = 'Jackie';
-            a.color = clr[rand(0, 6)];
-    
-            this.state.activities.push(a);
-        }
-    },
-
-    //Obtient une activité grâce à l'identifiant
-    get(id){
-        return this.state.activities.find(a => a.id === id);
     },
     
     actions:{
@@ -77,8 +89,8 @@ let store = {
         DELETE_ACTIVITIES: {
             
             applyLocally(params, store){
-                store.state.activities = store.state.activities.filter(a => {
-                    return params.indexOf(a) === -1;
+                store.state.activities[params.period] = store.state.activities[params.period].filter(a => {
+                    return params.activities.indexOf(a) === -1;
                 });
             },
             
@@ -103,9 +115,12 @@ let store = {
                 let self = this;
                 
                 request().success(function() {
-                    context.store.generateActivity();
+                    let period = context.params;
+                    
+                    Vue.set(context.store.state.activities, period, []);
+                    context.store.generateActivity(period);
                     result.isSuccess();
-                    self.loadedPeriods.push(context.params);
+                    self.loadedPeriods.push(period);
                     
                 });
             },
@@ -116,9 +131,4 @@ let store = {
         }
     }
     
-};
-
-
-Object.assign(store, ajaxStore);
-
-export default store;
+});
