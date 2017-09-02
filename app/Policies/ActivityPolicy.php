e8f05c0481214b5a5eaf92232b25fbda66a678db
Victor Michaux <victor.michaux@gmail.com>
2017-09-02 20:10:56 +0200

    Activity policies

---
+++ app/Policies/ActivityPolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Policies;
+
+use App\Activity;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ActivityPolicy
+{
+    use HandlesAuthorization;
+
+    public function show(User $user, Activity $activity) : bool
+    {
+        return $user->organizationOwnsActivity($activity);
+    }
+
+    public function update(User $user, Activity $activity) : bool
+    {
+        return $user->organizationOwnsActivity($activity);
+    }
+
+    public function destroy(User $user, Activity $activity) : bool
+    {
+        return $user->organizationOwnsActivity($activity);
+    }
+
+    public function storeSchedule(User $user, Activity $activity) : bool
+    {
+        return $user->organizationOwnsActivity($activity);
+    }
+
+    public function access(User $user, Activity $activity)
+    {
+        return $user->organizationOwnsActivity($activity);
+    }
+}
