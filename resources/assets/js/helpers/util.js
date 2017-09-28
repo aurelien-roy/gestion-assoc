d4f1ecc3d2d1f5a83e8aef271bc5f04393c5a8a1
johan-gras <johan.gras-sub@outlook.com>
2017-09-28 20:40:01 +0200

    Util : Fix deepCopy sur les valeurs null

--- resources/assets/js/helpers/util.js
+++ resources/assets/js/helpers/util.js
@@ -1,9 +1,14 @@
 window.deepCopy = function(o){
+    
+    if(o === null){
+        return null;
+    }
+    
     let output, v, key;
     output = Array.isArray(o) ? [] : {};
     for (key in o) {
         v = o[key];
         output[key] = (typeof v === "object") ? deepCopy(v) : v;
     }
-        return output;
+    return output;
 }
